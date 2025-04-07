80f258538cb52d1997cffa1b9f69cec87d56ab7a
YangHarvey <2998647748@qq.com>
2025-04-07 02:13:16 +0000

    clear exp1 & exp2 & add tests

--- exp1_flex/example/run.sh
+++
@@ -1,5 +0,0 @@
-flex example.l
-gcc lex.yy.c -o a
-./a < test.txt > myoutput.txt
-
-rm a lex.yy.c
--- exp2_yacc/demo-code/run.sh
+++
@@ -1,3 +0,0 @@
-bison -d parser.y      # 生成 parser.tab.c 和 parser.tab.h
-flex lex.l             # 生成 lex.yy.c
-gcc lex.yy.c parser.tab.c -o calc -lm
