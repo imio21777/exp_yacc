99a88d2da132a736fe10506c76e0f9ce0b1b2a52
imio21777 <yue22217@ruc.edu.cn>
2025-04-22 04:39:45 +0000

    Modify lex.l inast_impl

--- impl/ast_impl/compile.sh
+++ impl/ast_impl/compile.sh
@@ -1,11 +1,3 @@
-    # 清理
-    rm -f lex.yy.c yacc.tab.cpp yacc.tab.hpp parser *.o *.dot *.png
-
-    # 生成代码 (Bison 可能生成 .cpp 和 .hpp)
-    bison -d yacc.y -o yacc.tab.cpp --defines=yacc.tab.hpp
-    flex -o lex.yy.c lex.l
-
-    # 编译 (使用 g++)
-    g++ -std=c++11 main.cpp yacc.tab.cpp lex.yy.c -o parser -lfl
-
-    # （注意：文件名和 C++ 标准可能需要根据您的环境调整）
+make clean && make
+./parser ../../tests/asts_tests/testfile.txt > ast.dot
+# dot -Tpng ast.dot -o ast.png
